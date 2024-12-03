3dea232e232a3abb4af581f2b4d121703b32744e
Pavani Kosireddi <pavanikosireddi298@gmail.com>
2024-12-03 14:01:35 +0530

    committing all the changes

---
+++ src/main/java/Add.java
@@ -0,0 +1,13 @@
+public class Add {
+    public static void main(String[] args) {
+        // Fixed values
+        int x = 3;
+        int y = 5;
+
+        // Calculate the sum of the two numbers
+        int sum = x + y;
+
+        // Display the result
+        System.out.println("The sum of " + x + " and " + y + " is " + sum);
+    }
+}
